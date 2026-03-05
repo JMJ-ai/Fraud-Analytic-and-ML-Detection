53527b41d0d0796e74f4500039345a2750e546c7
Jenifer Mayang Jues <139893426+JMJ-ai@users.noreply.github.com>
2026-03-05 22:15:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -493,9 +493,13 @@ elif page == "ML Fraud Detection":
 elif page == "Model Evaluation":
 
     st.title("Model Evaluation")
-
-    X_test = test_df.drop("is_fraud", axis=1)
-    y_test = test_df["is_fraud"]
+    
+    id_cols =['transaction_id', 'customer_id', 'merchant_id']
+    suspcious_col = "post_auth_risk_score"
+    target = "is_fraud"
+    time_cols = "transaction_time"
+    X_test = test_df.drop(id_cols + [suspcious_col]+ [target] + [time_cols], axis=1)
+    y_test = test_df[target]
 
     X_proc = preprocessor.transform(X_test)
     X_proc = selector.transform(X_proc)

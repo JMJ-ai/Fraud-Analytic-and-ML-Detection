34e6d23644871d2b50202cb0498b596f3355e570
Jenifer Mayang Jues <139893426+JMJ-ai@users.noreply.github.com>
2026-03-16 14:38:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -750,33 +750,33 @@ elif nav == "Exploratory Data Analysis (EDA)":
             row=1,col=2
         )
 
-         fig.update_xaxes(
-                title_text="Hour"
-                title_font=dict(color="black"),
-                tickfont=dict(color="black"),
-                row=1, col=1
-            )
+        fig.update_xaxes(
+            title_text="Hour"
+            title_font=dict(color="black"),
+            tickfont=dict(color="black"),
+            row=1, col=1
+        )
 
-            fig.update_yaxes(
-                title_text="Fraud Rate",
-                title_font=dict(color="black"),
-                tickfont=dict(color="black"),
-                row=1, col=1
-            )
+        fig.update_yaxes(
+            title_text="Fraud Rate",
+            title_font=dict(color="black"),
+            tickfont=dict(color="black"),
+            row=1, col=1
+        )
 
-            fig.update_xaxes(
-                title_text="Date",
-                title_font=dict(color="black"),
-                tickfont=dict(color="black"),
-                row=1, col=2
-            )
+        fig.update_xaxes(
+            title_text="Date",
+            title_font=dict(color="black"),
+            tickfont=dict(color="black"),
+            row=1, col=2
+        )
 
-            fig.update_yaxes(
-                title_text="Fraud Rate",
-                title_font=dict(color="black"),
-                tickfont=dict(color="black"),
-                row=1, col=2
-            )
+        fig.update_yaxes(
+            title_text="Fraud Rate",
+            title_font=dict(color="black"),
+            tickfont=dict(color="black"),
+            row=1, col=2
+        )
 
         fig.update_layout(
                 plot_bgcolor="white",
